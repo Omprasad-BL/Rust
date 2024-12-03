69df87aa300fbe8620a0cefb63191c9366d79edf
Omprasadbl <omprasadbl@gmail.com>
2024-12-03 08:13:53 +0530

    rust repo

---
+++ tut1.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // Variables
+    let mut x=10;
+    print!("Hello world 1st code {} \n",x);
+
+    x=200;
+    print!("Hello world 1st code {} \n",x);
+    //CONSTANTS
+    const MAX_VALUE:f32= 100.0;
+    println!("Hello world 1st code {} \n",MAX_VALUE);
+
+    let num:i32=22; //declaring
+    //observe assigning 
+    let unum=num as f32;
+    print!("Hello world 1st code {} {}\n",num, unum)
+}
